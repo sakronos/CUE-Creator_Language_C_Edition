6ccac85125c403c6426e521ff95a0c9ce3967e27
sakronos <32067909+sakronos@users.noreply.github.com>
2018-01-09 23:09:59 +0800

    Add files via upload

---
+++ cue.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <io.h>
+#include <stdlib.h>
+int main()
+{
+    if ( !access("info.txt",0) )
+         puts("EXISITS!");
+    else
+         {
+             puts("DOESN'T EXISIT!");
+             FILE *fp;
+             fp=fopen("info.txt","w");
+             if(fp==NULL)
+             {
+                 printf("cannot open file \n");
+                 exit(0);
+             }
+             fputs("REM GENRE \n",fp);
+             fputs("REM DATE \n",fp);
+             fputs("TITLE \" \"\n",fp);
+             fclose(fp);
+         }
+    return 0;
+}
