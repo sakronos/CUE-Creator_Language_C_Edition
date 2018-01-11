a154a082b3208032f0efff62888a4eb02a1e70cb
sakronos <32067909+sakronos@users.noreply.github.com>
2018-01-11 23:04:16 +0800

    Add files via upload

--- cue.c
+++ cue.c
@@ -12,18 +12,51 @@ int main()
     else
          {
              puts("DOESN'T EXISIT!");
-             FILE *fp;
-             fp=fopen("info.txt","w");
-             if(fp==NULL)
+             FILE *fp11;
+             fp11=fopen("info.txt","w");
+             if(fp11==NULL)
              {
                  printf("cannot open file \n");
                  exit(0);
              }
-             fputs("REM GENRE \n",fp);
-             fputs("REM DATE \n",fp);
-             fputs("TITLE \" \"\n",fp);
-             fputs("PERFORMERofTRACK ",fp);
-             fclose(fp);
+             fclose(fp11);
+
+             FILE *fp12;
+             fp12=fopen("REM GENRE.txt","w");
+             if(fp12==NULL)
+             {
+                 printf("cannot open file \n");
+                 exit(0);
+             }
+             fclose(fp12);
+
+             FILE *fp13;
+             fp13=fopen("REM DATE.txt","w");
+             if(fp13==NULL)
+             {
+                 printf("cannot open file \n");
+                 exit(0);
+             }
+             fclose(fp13);
+
+             FILE *fp14;
+             fp14=fopen("TITLE.txt","w");
+             if(fp14==NULL)
+             {
+                 printf("cannot open file \n");
+                 exit(0);
+             }
+             fclose(fp14);
+
+             FILE *fp15;
+             fp15=fopen("PERFORMER of TRACK.txt","w");
+             if(fp15==NULL)
+             {
+                 printf("cannot open file \n");
+                 exit(0);
+             }
+             fclose(fp15);
+
          }
     return 0;
 }
